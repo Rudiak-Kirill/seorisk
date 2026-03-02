88d1f4cb0278c0af066cbeed8bbeb5382c4b3da5
Rudyak Kirill <rudyak.kirill@gmail.com>
2026-03-03 03:19:44 +0700

    Rename hero title

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-7 lg:text-left">
               <h1 className="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
-                SEO Risk Check
+                SEORISK
                 <span className="block text-orange-500">
                   Быстрый аудит SSR/ботов
                 </span>
