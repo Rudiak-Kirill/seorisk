d147658744c40ee406dc6562b43b77e0bb9d8618
Rudyak Kirill <rudyak.kirill@gmail.com>
2026-03-02 19:53:13 +0700

    Use ensureDb in queries

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -1,8 +1,10 @@
 import { desc, and, eq, isNull } from 'drizzle-orm';
-import { db } from './drizzle';
+import { ensureDb } from './drizzle';
 import { activityLogs, teamMembers, teams, users } from './schema';
 import { cookies } from 'next/headers';
 import { verifyToken } from '@/lib/auth/session';
+
+const db = ensureDb();
 
 export async function getUser() {
   const sessionCookie = (await cookies()).get('session');
